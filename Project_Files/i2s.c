721ba2c6d4939377d2e15910f9a4b406d42c3a33
idrisalghabra@gmail.com <irag500@cse169pc-39.cs.york.ac.uk>
2019-01-22 15:27:53 +0000

    Continued i2s

--- Project_Files/i2s.c
+++ Project_Files/i2s.c
@@ -1,7 +1,7 @@
 #include "i2s.h"
+//https://vle.york.ac.uk/bbcswebdav/pid-2848390-dt-content-rid-7066727_2/courses/Y2018-006400/2015-16/CMSIS/drivers/html/lpc17xx__i2s_8c_source.html
 
-
-void Init_I2S()
+void Init_I2S(uint8_t wordwidth,uint8_t)
 {
   GPDMA_Channel_CFG_Type GPDMACfg;
   I2S_MODEConf_Type Clock_Config;
@@ -10,5 +10,6 @@ void Init_I2S()
   //Assign
   LPC_PINCON->PINSEL0|=0x15540;//Set pins 5-9 as func 1
   I2S_Init(LPC_I2S);
-  LPC_
+  LPC_I2S->I2SDAI =;wordwidth|(mono<<2)|(stop<<3)|(reset<<4)|(1<<5)|((8*(wordwidth+1))<<6);
+  LPC_I2S->I2SDAO = ;
 }
