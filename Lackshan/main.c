f341ac4b3cd73f91a1580a178d92cb52ec055988
Lackshan Rama <lackshan.rama@gmail.com>
2019-01-24 11:20:59 +0000

    Wrote code to test SPI

--- Lackshan/main.c
+++ Lackshan/main.c
@@ -1,9 +1,19 @@
-#include "SPI.h"
+#include "spi.h"
+#include "SerialIO.h"
+#include "Delay.h"
+#include "lpc17xx_uart.h"
 
 int main(void) {
+    InitSerial();
+    WriteText("Initialising SSP\n\r");
     Init_SSP0();
-    u_int16_t Analog_Path = 0x816;
+    Init_SSP1();
+    uint8_t Analog_Path = 0x816;
+    WriteText("Writing on SSP0");
     SSP0_Write(&Analog_Path, 1);
-
+    uint8_t Input;
+    WriteText("Reading on SSP0");
+    SSP1_Read(&Input, 1);
+    WriteText((char)Input);
     return 0;
 }
