6502bc085fc0d265ad0dbb5f63b76571fdea620e
perlinwarp <perlinwarp@gmail.com>
2019-01-28 10:45:59 +0000

    New example code

---
+++ lpc176x/obj/mmc_176x_ssp.d
@@ -0,0 +1,9 @@
+obj/mmc_176x_ssp.o: mmc_176x_ssp.c LPC176x.h diskio.h ff.h ffconf.h
+
+LPC176x.h:
+
+diskio.h:
+
+ff.h:
+
+ffconf.h:
---
+++ lpc176x/obj/sound.d
@@ -0,0 +1,13 @@
+obj/sound.o: sound.c sound.h LPC176x.h ff.h ffconf.h xprintf.h uart176x.h
+
+sound.h:
+
+LPC176x.h:
+
+ff.h:
+
+ffconf.h:
+
+xprintf.h:
+
+uart176x.h:
