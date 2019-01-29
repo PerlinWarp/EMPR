5e44cdf05b18abfd366a24160dd95e0da2ca41d7
Brian Camacho <hello@briancamacho.me>
2019-01-29 13:41:18 +0000

    trying to fix Peter's dynamic linking

---
+++ SDExamp2/main.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include "rtc176x.h"
+#include "uart176x.h"
+#include "xprintf.h"
+#include "ff.h"
+#include "diskio.h"
+#include "sound.h"
+
+/* Read a text file and display it */
+
+FATFS FatFs;   /* Work area (filesystem object) for logical drive */
+
+int main (void)
+{
+    FIL fil;        /* File object */
+    TCHAR line[100]; /* Line buffer */
+    FRESULT fr;     /* FatFs return code */
+
+
+    /* Register work area to the default drive */
+    f_mount(&FatFs, "", 0);
+
+    /* Open a text file */
+    fr = f_open(&fil, "message.txt", FA_READ);
+    if (fr) return (int)fr;
+
+    /* Read every line and display it */
+    while (f_gets(line, sizeof(line), &fil)) {
+        printf(line);
+    }
+
+    /* Close the file */
+    f_close(&fil);
+
+    return 0;
+}
