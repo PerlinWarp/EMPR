a60710e261c58dac8330e5aebec0970a6502cddd
perlinwarp <perlinwarp@gmail.com>
2019-02-07 11:55:41 +0000

    Folder cleanup

--- SDExamp/main.c
+++ SDExamp/main.c
@@ -4,56 +4,34 @@
 #include <serial.h>
 #include <stdio.h>
 
-FRESULT scan_files (
-    char* path        /* Start node to be scanned (***also used as work area***) */
-)
-{
-    FRESULT res;
-    DIR dir;
-    UINT i;
-    static FILINFO fno;
-
-
-    res = f_opendir(&dir, path);                       /* Open the directory */
-    if (res == FR_OK) {
-        for (;;) {
-            res = f_readdir(&dir, &fno);                   /* Read a directory item */
-            if (res != FR_OK || fno.fname[0] == 0) break;  /* Break on error or end of dir */
-            if (fno.fattrib & AM_DIR) {                    /* It is a directory */
-                i = strlen(path);
-                sprintf(&path[i], "/%s", fno.fname);
-                res = scan_files(path);                    /* Enter the directory */
-                if (res != FR_OK) break;
-                path[i] = 0;
-            } else {
-                int n;
-                char buffer [50];                                      /* It is a file. */
-                n = sprintf(buffer,"%s/%s\n", path, fno.fname);
-                write_usb_serial_blocking(buffer, n);
-            }
-        }
-        f_closedir(&dir);
-    }
-
-    return res;
-}
-
-
-int main (void)
+int main(void)
 {
     serial_init();
-    write_usb_serial_blocking("///////", 7);
-
-    FATFS fs;
-    FRESULT res;
-    char buff[256];
-
-
-    res = f_mount(&fs, "", 0);
-
-    strcpy(buff, "/");
-    res = scan_files(buff);
     
+    FATFS FatFs;    // Work area (filesystem object) for logical drive
+    DIR dir;        // Directory object
+    FRESULT fr;
 
-    return res;
+    f_mount(&FatFs, "", 0);
+
+    fr = f_opendir(&dir, "/");
+    
+    int i;
+    FILINFO fi;
+    for (i=0; i < 10; i++){
+        fr = f_readdir(&dir, &fi);
+        if (fi.fattrib == AM_DIR){
+            write_usb_serial_blocking("FOLDER ", 7);
+            write_usb_serial_blocking(fi.fname, 13);
+        } else {
+            write_usb_serial_blocking("FILE ", 5);
+            write_usb_serial_blocking(fi.fname, 13);
+            write_usb_serial_blocking(",", 1);
+            write_usb_serial_blocking("1234", 4);
+            //write_usb_serial_blocking(fi.fsize, 13);
+        }
+        write_usb_serial_blocking("\r\n", 2);
+    }
+    
+    return 0;
 }
