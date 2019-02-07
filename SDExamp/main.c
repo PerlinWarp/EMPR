6ed95653fe30207d24660c2db1be19a598e5d3cc
perlinwarp <perlinwarp@gmail.com>
2019-02-07 12:31:41 +0000

    Reading from a file works, turns out you need to unmount the filesystem

--- SDExamp/main.c
+++ SDExamp/main.c
@@ -38,22 +38,40 @@ FRESULT scan_files (
     return res;
 }
 
+/* Read a text file and display it */
+
+FATFS FatFs;   /* Work area (filesystem object) for logical drive */
 
 int main (void)
 {
     serial_init();
     write_usb_serial_blocking("-", 1);
 
-    FATFS fs;
-    FRESULT res;
-    char buff[256];
+    FIL fil;        /* File object */
+    char line[100]; /* Line buffer */
+    FRESULT fr;     /* FatFs return code */
 
 
-    res = f_mount(&fs, "", 0);
+    /* Register work area to the default drive */
+    f_mount(&FatFs, "", 0);
 
-    strcpy(buff, "/");
-    res = scan_files(buff);
-    
+    /* Open a text file */
+    fr = f_open(&fil, "message.txt", FA_READ);
+    if (fr) return (int)fr;
 
-    return res;
+    /* Read every line and display it */
+    while (f_gets(line, sizeof line, &fil)) {
+         int n;
+        char buffer [250];                                      /* It is a file. */
+        n = sprintf(buffer,"%s\n", line);
+        write_usb_serial_blocking(buffer, n);
+    }
+
+    /* Close the file */
+    f_close(&fil);
+
+    //Unmount the file system
+    f_mount(0, "", 0); 
+    return 0;
 }
+
