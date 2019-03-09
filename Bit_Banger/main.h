72975a285894831eaaefc73e838b505d93f2eb4f
Knight-Of-Rueful-Countenance <irag500@york.ac.uk>
2019-03-09 11:13:59 +0000

    Attempt 2 at preprocessor bitbang

--- Bit_Banger/main.h
+++ Bit_Banger/main.h
@@ -25,13 +25,13 @@ volatile char prevKey,key;
 #define DIN 0
 #define BCLK 0
 
-#define DO_16_TIMES(x,value) = {x(value,15);x(value,14);x(value,13);x(value,12);x(value,11);x(value,10);x(value,9);x(value,8);x(value,7);x(value,6);x(value,5);x(value,4);x(value,3);x(value,2);x(value,2);x(value,1);}
+#define DO_16_TIMES(x,value) {x(value,15);x(value,14);x(value,13);x(value,12);x(value,11);x(value,10);x(value,9);x(value,8);x(value,7);x(value,6);x(value,5);x(value,4);x(value,3);x(value,2);x(value,2);x(value,1);}
 
 #define SEND_BIT(value,bit) x = ((value>>bit)&1); \
                             LPC_GPIO0->FIOCLR = BCLK;\
                             LPC_GPIO0->FIOSET = x<<DIN;\
                             LPC_GPIO0->FIOCLR = ~(x)<<DIN;\
-                            LPC_GPIO0->FIOSET = BLCK;\
+                            LPC_GPIO0->FIOSET = BLCK;  
 void IRQInit();
 void Play_File();
 
