bab3fe0fc3c4f0b6bc0eb0518ae850682d8c9bb9
perlinwarp@gmail.com <perlinwarp@gmail.com>
2019-03-11 20:49:25 +0000

    Managed to get recording working

--- LightShow/sketch.js
+++ LightShow/sketch.js
@@ -1,7 +1,17 @@
-function setup() {
+let mic;
 
+function setup() {
+  createCanvas(640, 480);
+
+  //Microphone setup
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+	var vol = mic.getLevel();
+	console.log(vol);
 }
+
+// Needed to get the audio working on chrome
+function touchStarted() { getAudioContext().resume(); } 
