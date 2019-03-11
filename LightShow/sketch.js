04a316b9961e2790f8a9a3ab4ae50fe20d5be045
perlinwarp@gmail.com <perlinwarp@gmail.com>
2019-03-11 21:35:23 +0000

    Changing the fill of a rectangle based on mic input

--- LightShow/sketch.js
+++ LightShow/sketch.js
@@ -1,4 +1,5 @@
 let mic;
+var button;
 
 function setup() {
   createCanvas(640, 480);
@@ -11,6 +12,10 @@ function setup() {
 function draw() {
 	var vol = mic.getLevel();
 	console.log(vol);
+	fill(int(vol*255));
+	rect(30, 20, 55, 55);
+
+
 }
 
 // Needed to get the audio working on chrome
