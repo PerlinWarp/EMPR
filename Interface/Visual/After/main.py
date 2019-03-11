3969f11ba797cd02d8290a1f3d021697a3cf5925
perlinwarp <perlinwarp@gmail.com>
2019-03-11 10:04:00 +0000

    Changing the GUI

--- Interface/Visual/After/main.py
+++ Interface/Visual/After/main.py
@@ -4,6 +4,13 @@ from tkinter import *
 
 class Timer:
     def __init__(self, parent):
+        #Serial Setup
+        self.w = Canvas(root, width=200, height=100)
+        self.w.pack()
+
+        self.w.create_rectangle(10,10,100,20, fill="red")
+
+
         # variable storing time
         self.seconds = 0
         # label displaying time
@@ -21,7 +28,16 @@ class Timer:
         # request tkinter to call self.refresh after 1s (the delay is given in ms)
         self.label.after(1000, self.refresh_label)
 
+        self.w.delete("all")
+        self.w.create_rectangle(10,10,100,20*self.seconds, fill="red")
+
+    ##def update_volume(self):
+
+
 if __name__ == "__main__":
     root = Tk()
+    root.geometry("800x600")
     timer = Timer(root)
     root.mainloop()
+
+
