7d7b0b452d01d0a8c60b504ad5c78291a76191ca
Brian Camacho <hello@briancamacho.me>
2019-03-13 01:29:37 +0000

    finish tkcancer

--- Interface/Effects/main.py
+++ Interface/Effects/main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 from effects import *
-
+from PIL import Image
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
@@ -13,9 +13,7 @@ class Application(tk.Frame):
         self.selected_file_path = None
         self.console = None
 
-
     def create_widgets(self):
-
         self.choose_file = tk.Button(self)
         self.choose_file["text"] = "Choose File"
         self.choose_file["command"] = self.choose_file_cmd
@@ -25,6 +23,16 @@ class Application(tk.Frame):
         self.filter["text"] = "Filter"
         self.filter["command"] = self.filter_cmd
         self.filter.pack(side="right")
+
+        self.delay = tk.Button(self)
+        self.delay["text"] = "Delay"
+        self.delay["command"] = self.delay_cmd
+        self.delay.pack(side="right")
+
+        self.flange = tk.Button(self)
+        self.flange["text"] = "Flange"
+        self.flange["command"] = self.flange_cmd
+        self.flange.pack(side="right")
 
         self.save = tk.Button(self)
         self.save["text"] = "Save"
@@ -36,7 +44,7 @@ class Application(tk.Frame):
         self.quit.pack(side="bottom")
 
     def save_cmd(self):
-        self.console.save("result.wav")
+        self.console.save("applied_effects.wav")
 
     def filter_cmd(self):
         self.console.effectFilter(5)
@@ -57,6 +65,7 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("800x600")
     root.attributes('-type', 'dialog')
+
     app = Application(master=root)
     app.mainloop()
 
