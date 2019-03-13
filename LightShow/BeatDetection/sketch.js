438a8e24374e23ab2ae7c82adbecd8c8f0c7978a
perlinwarp@gmail.com <perlinwarp@gmail.com>
2019-03-13 09:22:11 +0000

    You then need to decay the threshold

--- LightShow/BeatDetection/sketch.js
+++ LightShow/BeatDetection/sketch.js
@@ -1,13 +1,19 @@
 var mic;
+// The minumum amplitude requires to trigger a wave
 var thres = 0.11;
+//A variable that stops it triggering all the time, decays over time
+var cutOff = 0.11;
+
+// We need to keep track of time for the decay
+var framesSinceBeat = 0; 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
-  //Get input from the mic
+  // Get input from the mic
   mic = new p5.AudioIn();
   mic.start();
-  //Fixing the permissions issue on chrome
+  // Fixing the permissions issue on chrome
   touchStarted();
 }
 
@@ -25,10 +31,22 @@ function draw() {
 function touchStarted() { getAudioContext().resume(); } 
 
 function detectBeat(vol){
-  if(vol > thres){
+  if(vol > thres && vol > cutOff){
+
     backgroundColor = color( random(0,255), random(0,255), random(0,255) );
     background(backgroundColor);
-    thres = thres * 1.1;
+
+    cutOff = vol * 1.2;
+    framesSinceBeat = 0;
+  }else{
+    if(framesSinceBeat <= 30){
+      framesSinceBeat++;
+    }else{
+      cutOff *= 0.9;
+      // We dont want to trigger a beat at very low volumes
+      // Even if one has not been heard for a while
+      cutOff = Math.max(thres, cutOff);
+    }
   }
 
 }
